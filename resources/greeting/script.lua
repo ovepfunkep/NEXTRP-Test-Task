5cf9f51c975b04e6eb5800c85e55634d14479d8c
Ivan Tyabin <Ivan_Tyabin@mail.ru>
2023-08-28 22:24:29 +0700

    removed unneseccary logic

--- resources/greeting/script.lua
+++ resources/greeting/script.lua
@@ -1,7 +1,4 @@
 function joinHandler()
-	spawnPlayer(source,2280,-1261,23)
-	fadeCamera(source, true)
-	setCameraTarget(source, source)
 	outputChatBox ( "Hello world!")
 end
 addEventHandler("onPlayerJoin", getRootElement(), joinHandler)
