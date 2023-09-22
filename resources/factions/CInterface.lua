e2b22f202035bcf038db34f41a3e0edc2912deb8
Ivan Tyabin <Ivan_Tyabin@mail.ru>
2023-09-22 17:21:13 +0700

    added factions realization.

---
+++ resources/factions/CInterface.lua
@@ -0,0 +1,182 @@
+playerFaction = nil
+factionPlayers = {}
+tabPanel = nil
+managementTab = nil
+listTab = nil
+playerList = {}
+
+addEvent("becameLeader", true)
+function changeInterfaceToLeaders()
+	destroyElement(tabPanel)
+	createFactionLeaderInterface()
+end
+addEventHandler("becameLeader", getRootElement(), changeInterfaceToLeaders)
+
+function createFactionLeaderInterface()
+	local X = 0.35
+	local Y = 0.25
+	local Width = 0.3
+	local Height = 0.5
+	tabPanel = guiCreateTabPanel(X, Y, Width, Height, true)
+	
+	listTab = guiCreateTab("Player list", tabPanel)
+	X = 0.05
+	Y = 0.05
+	Width = 0.3
+	Height = 0.75
+	playerList = guiCreateGridList(X, Y, Width, Height, true, listTab)
+	guiGridListAddColumn(playerList, "Player", 1)
+	for k,v in ipairs(factionPlayers) do
+		guiGridListAddRow(playerList, getElementData(v, "fullName"))
+	end
+
+	if playerFaction.Id == "city_mayor" then 
+		managementTab = guiCreateTab("City management", tabPanel)
+	end
+	X = 0.05
+	Y = 0.825
+	Width = 0.3
+	Height = 0.1
+	buttonFire = guiCreateButton(X, Y, Width, Height, "Fire", true, listTab)
+
+	function fireSelectedPlayer()
+		r, c = guiGridListGetSelectedItem(playerList)
+		fullName = guiGridListGetItemText(playerList, r, c)
+		_, i = string.find(fullName, "]")
+		id = tonumber(string.sub(fullName, 2, i - 1))
+		triggerServerEvent("CFirePlayerById", localPlayer, id)
+	end
+	addEventHandler("onClientGUIClick", buttonFire, fireSelectedPlayer)
+
+	X = 0.5
+	Y = 0.35
+	Width = 0.3
+	Height = 0.05
+	editBox = guiCreateEdit(X, Y, Width, Height, "Player Id", true, listTab)
+	X = 0.6
+	Y = 0.42
+	Width = 0.1
+	Height = 0.05
+	inviteButton = guiCreateButton(X, Y, Width, Height, "Invite", true, listTab)
+	function invitePlayer()
+		playerId = tonumber(guiGetText(editBox))
+		triggerServerEvent("CInvitePlayer", localPlayer, playerId, playerFaction) 
+	end
+	addEventHandler("onClientGUIClick", inviteButton, invitePlayer)
+	
+	guiSetVisible(tabPanel, false)
+end
+
+function createFactionMemberInterface()
+	local X = 0.42
+	local Y = 0.25
+	local Width = 0.15
+	local Height = 0.4
+	tabPanel = guiCreateTabPanel(X, Y, Width, Height, true)
+	
+	listTab = guiCreateTab("Player list", tabPanel)
+	X = 0.15
+	Y = 0.05
+	Width = 0.7
+	Height = 0.8
+	playerList = guiCreateGridList(X, Y, Width, Height, true, listTab)
+	guiGridListAddColumn(playerList, "Player", 1)
+	for k,v in ipairs(factionPlayers) do
+		guiGridListAddRow(playerList, getElementData(v, "fullName"))
+	end
+	
+	guiSetVisible(tabPanel, false)
+end
+
+addEvent("addedToFaction", true)
+function addFactionInterface()
+	triggerServerEvent("CGetPlayerFactionData", localPlayer)
+end	
+addEventHandler("addedToFaction", localPlayer, addFactionInterface)
+
+setElementData(localPlayer, "factionId", false) -- need for debugging (on resource restart)
+if getElementData(localPlayer, "factionId") ~= false then addFactionInterface() end
+
+addEvent("sentPlayerFactionData", true)
+function setPlayerFaction(faction, factionPlayersData)
+	playerFaction = faction
+	factionPlayers = factionPlayersData
+	if playerFaction.Leader == localPlayer then createFactionLeaderInterface() 
+	else createFactionMemberInterface() end
+	bindKey("p", "down", toogleInterface)
+end
+addEventHandler("sentPlayerFactionData", root, setPlayerFaction)
+
+function toogleInterface()
+	guiSetVisible(tabPanel, not guiGetVisible(tabPanel))
+	showCursor(not isCursorShowing(localPlayer))
+end
+
+addEvent("playerFired", true)
+function removeFactionInterface()
+	destroyElement(tabPanel)
+	unbindKey("p", "down", toogleInterface)
+	showCursor(false)
+end
+addEventHandler("playerFired", localPlayer, removeFactionInterface)
+
+addEvent("removePlayerFromList", true)
+function removePlayer()
+	for pos, v in ipairs(factionPlayers) do
+		if getElementData(v, "factionId") == false then 
+			table.remove(factionPlayers, pos)
+			local playerFullName = getElementData(v, "fullName")  
+			for p, val in ipairs(playerList) do
+				if val == playerFullName then guiGridListRemoveRow(playerList, p) break end 
+			end 
+			break 
+		end
+	end
+end
+addEventHandler("removePlayerFromList", localPlayer, removePlayer)
+
+addEvent("playerInvited", true)
+function createInvitationWindow(faction)
+	X = 0.35
+	Y = 0.4
+	Width = 0.3
+	Height = 0.1
+	window = guiCreateWindow(X, Y, Width, Height, "Invitation", true)
+
+	X = 0.1
+	Y = 0.3
+	Width = 0.9
+	Height = 0.2
+	headerText = guiCreateLabel( X, Y, Width, Height, getPlayerName(source) .. " offers you to become a member of " .. faction.Name, true, window)
+
+	X = 0.1
+	Y = 0.6
+	Width = 0.35
+	Height = 0.5
+	buttonAccept = guiCreateButton( X, Y, Width, Height, "Accept", true, window )
+		
+	function acceptInviteOffer()
+		triggerServerEvent("COfferAccepted", localPlayer, faction)
+		destroyElement(window)
+	end
+	addEventHandler("onClientGUIClick", buttonAccept, acceptInviteOffer)
+
+	X = 0.5
+	Y = 0.6
+	Width = 0.35
+	Height = 0.5
+	buttonDecline = guiCreateButton( X, Y, Width, Height, "Decline", true, window )
+		
+	function declineInviteOffer()
+		destroyElement(window)
+	end
+	addEventHandler("onClientGUIClick", buttonDecline, declineInviteOffer)
+end
+addEventHandler("playerInvited", localPlayer, createInvitationWindow)
+
+addEvent("addedNewPlayer")
+function addPlayerToList()
+	table.insert(factionPlayers, #list+1, source)
+	guiGridListAddRow(playerList, getElementData(source, "fullName"))
+end
+addEventHandler("addedNewPlayer", localPlayer, addPlayerToList)
---
+++ resources/factions/SController.lua
@@ -0,0 +1,112 @@
+factions = {}
+
+function findPlayerById(playerId)
+	local players = getElementsByType("player")
+	for k, player in ipairs(players) do 
+		if getElementData(player, "id") == playerId then return player end
+	end
+end
+
+function findFactionById(stringId)
+	for k, faction in ipairs(factions) do 
+		if faction.Id == stringId then return faction end
+	end
+end
+
+function getFactionPlayers(factionId)
+	local players = {}
+	for k,v in ipairs(getElementsByType("player")) do
+		if getElementData(v, "factionId") == factionId then table.insert(players, v) end
+	end
+	return players
+end
+
+function createFaction(playerSource, commandName, name, stringId)
+	if stringId and name and not findFactionById(stringId) then
+    	local newFaction = {}
+		newFaction.Id = stringId
+		newFaction.Name = name
+		newFaction.PlayersIds = {} -- не захотел хранить кучу данных о пользователях
+		newFaction.Leader = 0
+		table.insert(factions, newFaction)
+		outputChatBox("Successfully created faction.")
+	else outputChatBox("You can't create a faction.") end
+end
+addCommandHandler("faction_create", createFaction)
+
+function firePlayer(player)
+	local playerFactionId = getElementData(player, "factionId")
+	if not playerFactionId then return end -- if player doesn't have faction
+	
+	local faction = findFactionById(playerFactionId)
+
+	local position = 0
+	for k, val in ipairs(faction.PlayersIds) do
+		if val == playerId then position = k break end
+	end
+
+	if faction.Leader = player then faction.Leader = nil end 
+
+	table.remove(faction.PlayersIds, position)
+	setElementData(player, "factionId", false)
+	triggerClientEvent(player, "playerFired", root) -- remove interface from player
+	triggerClientEvent(getFactionPlayers(playerFactionId), "removePlayerFromList", root) -- clear player from faction players list
+end
+
+addEvent("CFirePlayerById", true)
+function firePlayerById(id)
+	player = findPlayerById(id)
+	firePlayer(player)
+end
+addEventHandler("CFirePlayerById", root, firePlayerById)
+
+function setPlayerFaction(playerSource, commandName, playerId, factionId)
+	local playerFactionId = getElementData(playerSource, "factionId")
+
+	if playerFactionId == factionId then return end
+
+	firePlayer(playerSource) -- remove from another faction if invited to new or if factionId == nil
+	
+	if factionId then 
+		table.insert(findFactionById(factionId).PlayersIds, playerId)
+		setElementData(playerSource, "factionId", factionId)
+		triggerClientEvent(playerSource, "addedToFaction", playerSource) -- add faction interface
+		triggerClientEvent(getFactionPlayers(factionId), "addedNewPlayer", playerSource) -- update members list 
+		outputChatBox("Successfully added player to faction.")
+	end
+end
+addCommandHandler("set_player_faction", setPlayerFaction)
+
+function setPlayerFactionLeader(playerSource, commandName, playerId, factionId)
+	player = findPlayerById(tonumber(playerId))
+	findFactionById(factionId).Leader = player
+	setPlayerFaction(playerSource, commandName, playerId, factionId)
+	triggerClientEvent(player, "becameLeader", playerSource)
+	outputChatBox("Successfully set new faction leader.")
+end
+addCommandHandler("set_player_faction_leader", setPlayerFactionLeader)
+
+function sendMessageToFaction(playerSource, commandName, text)
+	local outputText = getElementData(playerSource, "fullName") .. ": " .. text
+	outputChatBox(outputText, getFactionPlayers(getElementData(playerSource, "factionId")), 0, 0, 255)
+end
+addCommandHandler("f", sendMessageToFaction)
+
+addEvent("CGetPlayerFactionData", true)
+function sendPlayerFactionToClient()
+	factionId = getElementData(source, "factionId")
+	triggerClientEvent(source, "sentPlayerFactionData", source, findFactionById(factionId), getFactionPlayers(factionId))
+end
+addEventHandler("CGetPlayerFactionData", getRootElement(), sendPlayerFactionToClient)
+
+addEvent("CInvitePlayer", true)
+function invitePlayerToFaction(playerId, faction)
+	triggerClientEvent(findPlayerById(playerId), "playerInvited", source, faction)
+end
+addEventHandler("CInvitePlayer", root, invitePlayerToFaction)
+
+addEvent("COfferAccepted", true)
+function addInvitedPlayer(faction)
+	setPlayerFaction(source, nil, getElementData(source, "id"), faction.Id)
+end
+addEventHandler("COfferAccepted", root, addInvitedPlayer)
